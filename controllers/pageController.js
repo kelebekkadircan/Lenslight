a20374daaf6c3fa0c2c6154275e25ceafe620f6f
kelebekkadircan <kelebekkadircan@gmail.com>
2023-09-18 16:38:57 +0300

    day8_JWT_AUTH

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -20,4 +20,12 @@ const getRegisterPage = (req, res) => {
     })
 }
 
-export { getIndexPage, getAboutPage, getRegisterPage }
+const getLoginPage = (req, res) => {
+
+    res.render("login", {
+        link: "login"
+    })
+}
+
+
+export { getIndexPage, getAboutPage, getRegisterPage, getLoginPage }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/userModel.js";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const createUser = async (req, res) => {
 
@@ -22,7 +24,59 @@ const createUser = async (req, res) => {
 
 }
 
+const loginUser = async (req, res) => {
+
+
+    try {
+        const { username, password } = req.body
+
+        const user = await User.findOne({ username })
+
+        let same = false;
+
+        if (user) {
+            same = await bcrypt.compare(password, user.password)
+        }
+        else {
+            return res.status(401).json({
+                succeded: false,
+                error: "There is no such user"
+            })
+        }
+
+        if (same) {
+            res.status(200).json({
+                user,
+                token: createToken(user._id)
+            })
+
+        }
+        else {
+            res.status(401).json({
+                succeded: false,
+                error: "Password are not matched"
+            })
+        }
 
 
 
-export { createUser };
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        })
+    }
+
+}
+
+
+const createToken = (userId) => {
+
+    return jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    })
+}
+
+
+
+export { createUser, loginUser };
